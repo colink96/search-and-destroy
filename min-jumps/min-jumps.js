33130eff6eafc34a0df93b539b06abada1137f58
Colin Kim <ck2344@nyu.edu>
2019-10-23 16:28:08 -0400

    finished min-jumps

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,18 @@
 'use strict';
 
 // Complete this algo
-const minJumps = arr => {
-
+const minJumps = (arr, index = 0, jumps = 0) => {
+  const start = arr[index];
+  if (start >= arr.slice(index).length - 1) {
+    jumps += 1;
+    return jumps;
+  } else {
+    const possibleJumps = arr.slice(index + 1, index + start + 1);
+    let largestJump = Math.max(...possibleJumps);
+    let largestJumpIndex =
+      possibleJumps.findIndex(val => val === largestJump) + index + 1;
+    return minJumps(arr, largestJumpIndex, jumps + 1);
+  }
 };
 
-module.exports = minJumps
+module.exports = minJumps;
