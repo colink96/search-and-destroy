7b578af27b3421556e2e798a4a6b6fb3b032a7d5
Victor <Vguerrero42@gmail.com>
2019-10-23 15:33:35 -0400

    big brain algos

--- looper/isloop.js
+++ looper/isloop.js
@@ -1,10 +1,17 @@
-'use strict';
+"use strict";
 
 //Complete this algo
-const isLoop = (linkedlist) => {
-
+const isLoop = (linkedList, memo = {}, idx = 1) => {
+  const node = linkedList.getNthNode(idx);
+  console.log("node:", node.value, idx);
+  if (!(node.value in memo) && !node.next) {
+    return false;
+  } else if (node.value in memo) return true;
+  else {
+    memo[node.value] = true;
+    return isLoop(linkedList, memo, idx + 1);
+  }
 };
-
 
 /*
 EXTRA CREDIT:
@@ -14,4 +21,4 @@ This function should return the Node value the loop begins at
 Remember to write some test specs too!
 
 */
-module.exports = isLoop
+module.exports = isLoop;
